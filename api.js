f14bdf1aacd705190b180fe579465324369400f2
Luciano Ratamero <luciano@ratamero.com>
2019-07-09 16:20:46 -0300

    Add default request headers to API helpers.

--- api.js
+++ api.js
@@ -1,9 +1,17 @@
 
-export function fetchFromApi(url, init={ method: 'GET' }) {
+export function fetchFromApi(url, requestData={ method: 'GET' }) {
+
+  if (!requestData.headers) {
+    requestData.headers = {};
+  }
+
+  if(!requestData.headers['content-type']){
+    requestData.headers['content-type'] = 'application/json'
+  }
 
   return new Promise((resolve, reject) => {
 
-    fetch(buildRequest(url, init))
+    fetch(buildRequest(url, requestData))
       .then(response => {
 
         // here, we prepare fetch to reject when the status is 4xx or above
@@ -18,7 +26,7 @@ export function fetchFromApi(url, init={ method: 'GET' }) {
 
 }
 
-export function buildRequest(url, init) {
-  const request = new Request(url, { ...init });
+export function buildRequest(url, requestData) {
+  const request = new Request(url, { ...requestData });
   return request;
 }
