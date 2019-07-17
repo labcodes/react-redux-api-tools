61da021fd6293adb2fbe27e3873b858013529ed3
David Costa <davidcostadev@gmail.com>
2019-07-17 09:16:02 -0300

    Fix lint

--- api.test.js
+++ api.test.js
@@ -8,39 +8,35 @@ describe('fetchFromApi', () => {
   it('should resolve fetch and return data with error', async () => {
     fetch.mockResolvedValue({
       status: 400,
-      message: 'The name is empty',
+      message: 'The name is empty'
     });
 
     const header = {
       method: 'POST',
-      body: { name: '' },
+      body: { name: '' }
     };
     try {
       await fetchFromApi('http://localhost:3000/products');
     } catch (e) {
       expect(e).toEqual({
         status: 400,
-        message: 'The name is empty',
+        message: 'The name is empty'
       });
     }
   });
 
   it('should resolve fetch and return data', async () => {
     fetch.mockResolvedValue({
-      data: [
-        { name: 'Xbox' },
-      ],
+      data: [{ name: 'Xbox' }]
     });
 
     const response = await fetchFromApi('http://localhost:3000/products');
 
     expect(response).toEqual({
-      data: [
-        { name: 'Xbox' },
-      ],
+      data: [{ name: 'Xbox' }]
     });
   });
-  
+
   it('should reject fetch and catch by error', async () => {
     fetch.mockRejectedValue(new Error('404 Not found'));
     try {
